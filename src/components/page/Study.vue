819077360d807e23aa132d0062c56cb81ae94edc
seer300 <aasd990@126.com>
2024-03-18 15:51:40 +0800

    技术学习页面完善；

--- src/components/page/Study.vue
+++ src/components/page/Study.vue
@@ -1,12 +1,61 @@
 <template>
-    <h1>添加案例页面</h1>
+<div>
+    <el-card class="box-card box-card2">
+        <div slot="header" class="clearfix">
+            <span>10个汽车维修案例(汽车发动机维修难点)</span>
+        </div>
+        <div>
+            <i class="el-icon-share"></i>
+            <a href="https://wenku.baidu.com/view/555a55f2fc4ffe473268ab1a.html?fr=income1-doc-search&_wkts_=1710747595793&wkQuery=%E6%B1%BD%E8%BD%A6%E7%BB%B4%E4%BF%AE%E6%A1%88%E4%BE%8B&needWelcomeRecommand=1">查看PDF</a>
+        </div>
+    </el-card>
+
+    <el-card class="box-card box-card2">
+        <div slot="header" class="clearfix">
+            <span>汽车空调故障排除流程</span>
+        </div>
+        <div>
+            <iframe src="//player.bilibili.com/player.html?aid=40640254&bvid=BV1Mt411H71H&cid=97403750&p=1" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+        </div>
+    </el-card>
+
+    <el-card class="box-card box-card2">
+        <div slot="header" class="clearfix">
+            <span>汽车维修100个案例分析报告</span>
+            <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
+        </div>
+        <div>
+            <i class="el-icon-share"></i>
+            <a href="https://wenku.baidu.com/view/15884625bbf3f90f76c66137ee06eff9aff84928.html">查看PDF</a>
+        </div>
+    </el-card>
+
+    <el-card class="box-card box-card2">
+        <div slot="header" class="clearfix">
+            <span>起动机拆卸（视频）</span>
+        </div>
+        <div>
+            <iframe src="//player.bilibili.com/player.html?aid=40640254&bvid=BV1Mt411H71H&cid=97405163&p=4" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+        </div>
+    </el-card>
+
+    <el-card class="box-card box-card2">
+        <div slot="header" class="clearfix">
+            <span>AC350加注机介绍和管道连接（视频）</span>
+            <!-- <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button> -->
+        </div>
+        <div>
+            <iframe src="//player.bilibili.com/player.html?aid=40640254&bvid=BV1Mt411H71H&cid=72095721&p=12" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>
+        </div>
+    </el-card>
+</div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            rows: [] // 存储从后端获取的随机五行数据
+            
         };
     },
     mounted() {
@@ -18,4 +67,9 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.box-card2{
+    margin-top: 15px;
+}
+
+</style>
